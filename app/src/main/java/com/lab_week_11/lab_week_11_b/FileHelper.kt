6a8af6bc69933334b9bbb1c7b748109cf92a0e42
MATYUS05 <147257577+MATYUS05@users.noreply.github.com>
2025-11-21 17:20:02 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStor

---
+++ app/src/main/java/com/lab_week_11/lab_week_11_b/FileHelper.kt
@@ -0,0 +1,22 @@
+package com.lab_week_11.lab_week_11_b
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+
+class FileHelper(private val context: Context) {
+
+    fun getUriFromFile(file: File): Uri {
+        return FileProvider.getUriForFile(
+            context, context.packageName + ".provider", file
+        )
+    }
+
+    fun getPicturesFolder(): String =
+        Environment.DIRECTORY_PICTURES
+
+    fun getVideosFolder(): String =
+        Environment.DIRECTORY_MOVIES
+}
